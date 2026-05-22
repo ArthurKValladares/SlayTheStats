d9e1c42159a7a1ef4e26ee65a9fab99945a59738
ArthurKValladares <arthurkaukal@gmail.com>
2026-05-22 16:43:05 -0300

    get hover tips patch working

---
+++ SlayTheStatsCode/Patches/CardModelHoverTipsPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+
+namespace SlayTheStats.SlayTheStatsCode.Patches;
+
+[HarmonyPatch(typeof(CardModel), nameof(CardModel.HoverTips), MethodType.Getter)]
+public static class CardModelHoverTipsPatch
+{
+    public static void Postfix(CardModel __instance, ref IEnumerable<IHoverTip> __result)
+    {
+        if (__instance.IsCanonical) return;
+
+        // Construct HoverTip
+        var tip = new HoverTip();
+        object boxed = tip;
+        AccessTools.Property(typeof(HoverTip), nameof(HoverTip.Title)).SetValue(boxed, "Test");
+        AccessTools.Property(typeof(HoverTip), nameof(HoverTip.Description)).SetValue(boxed, "This is a test");
+        tip = (HoverTip)boxed;
+        
+        var list = __result.ToList();
+        list.Add(tip);
+        __result = list;
+    }
+}
---
+++ SlayTheStatsCode/Patches/RelicModelHoverTipsPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+
+namespace SlayTheStats.SlayTheStatsCode.Patches;
+
+[HarmonyPatch(typeof(RelicModel), nameof(RelicModel.HoverTips), MethodType.Getter)]
+public static class RelicModelHoverTipsPatch
+{
+    public static void Postfix(RelicModel __instance, ref IEnumerable<IHoverTip> __result)
+    {
+        if (__instance.IsCanonical) return;
+
+        // Construct HoverTip
+        var tip = new HoverTip();
+        object boxed = tip;
+        AccessTools.Property(typeof(HoverTip), nameof(HoverTip.Title)).SetValue(boxed, "Test");
+        AccessTools.Property(typeof(HoverTip), nameof(HoverTip.Description)).SetValue(boxed, "This is a test");
+        tip = (HoverTip)boxed;
+        
+        var list = __result.ToList();
+        list.Add(tip);
+        __result = list;
+    }
+}
